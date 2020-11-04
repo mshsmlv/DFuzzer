5f571adb4b3b5b2df5d7dddb140da790fb73d590
MashaSamoylova <mashanedyak@gmail.com>
2020-11-04 12:56:36 +0700

    Add variables from another contexts

--- mutator/node_generator.js
+++ mutator/node_generator.js
@@ -2,8 +2,6 @@ var esprima = require('esprima');
 var escope = require('escope');
 var estraverse = require('estraverse');
 var escodegen = require('escodegen');
-var esquery = require('esquery')
-
 
 // https://esprima.readthedocs.io/en/latest/syntax-tree-format.html - вся инфа по возможным нодам
 
@@ -28,6 +26,38 @@ function randomChoice(arr) {
 // типо если в блоке continue то его нельзя пихать.
 // то есть замутить контексты.
 
+function __get_node(ast, aimed_type, index) {
+    var scopeManager = escope.analyze(ast);
+        var res_nodes = []
+        estraverse.traverse(ast, {
+            enter: function (node, parent) {
+                if (node.type == aimed_type) {
+                    var variables = []
+                    if (parent) {
+                        var parent_scope =  scopeManager.acquire(parent);
+                        if (parent_scope) {
+                            variables = parent_scope.variables;
+                        }                  
+                    } 
+                    res_nodes.push(
+                    {
+                        node: node,
+                        variables: variables, 
+                    })
+                }
+            },
+            leave: function (node, parent) {
+            }
+        });
+        if (!index) {
+            index = Math.floor(Math.random() * res_nodes
+            .length);
+        };
+
+        console.log("index:", index);
+        return [res_nodes[index], ast];
+}
+
 function getNode(aimed_type) {
     while (true){ 
         var tree_file = "./data-set/basic/" + randomChoice(trees);
@@ -38,14 +68,13 @@ function getNode(aimed_type) {
         } catch(e) {
             continue
         }
-        
-        nodes = esquery.query(ast, aimed_type)
-        if (nodes.length == 0) {
-            continue
+        console.log(tree_file);
+        var [new_node, source_tree] = __get_node(ast, aimed_type);
+        if (new_node) {
+            return [new_node, source_tree];
         }
-        random_index = Math.floor(Math.random() * nodes.length);
-        console.log(tree_file, random_index);
-        return [nodes[random_index], ast];
+        continue;
+         
     }   
 }
 
@@ -53,25 +82,7 @@ function getSpecifiedNode(tree_file, node_index, aimed_type) {
     var code = fs.readFileSync(tree_file, "utf-8");
     var ast = esprima.parse(code);
 
-    var scopeManager = escope.analyze(ast);
-    var global_variables = scopeManager.acquire(ast).variables;
-    console.log(global_variables);
-    console.log(escodegen.generate(ast));
-
-    estraverse.traverse(ast, {
-        enter: function (node, parent) {
-                console.log("~~~~~~~~~~~~~~~~~~~~~~~~~");
-                console.log(node)
-                console.log("\n\n\nVariables: ")
-                console.log(scopeManager.acquire(node));
-
-        },
-        leave: function (node, parent) {
-        }
-    });
-
-    nodes = esquery.query(ast, aimed_type);
-    return [nodes[node_index], ast];
+    return __get_node(ast, aimed_type, node_index);
 }
 
 function check_variable_existence(var_array, varname) {
@@ -88,6 +99,8 @@ function change_blocks(ast, scopeManager) {
 
     estraverse.replace(ast, {
         enter: function (node, parent) {
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
             switch (node.type) {
                 case "FunctionDeclaration":
                 case  "ReturnStatement":
@@ -104,11 +117,9 @@ function change_blocks(ast, scopeManager) {
                 case 1: // generate
             }
         
-            var [new_node, source_tree] = getSpecifiedNode("./data-set/basic/testMulOverflow.js", 0, node.type);
+           // var [new_node, source_tree] = getSpecifiedNode("./data-set/basic/testMulOverflow.js", 0, node.type);
             
-            
-
-             // var [new_node, source_tree] = getNode(node.type);
+            var [new_node, source_tree] = getNode(node.type);
             // при вставке дерева нужно проходиться по декларациям функций и классов.
             // то есть если вызываем какую-то функцию, то ее декларацию в дерево тоже впихиваем.
             //
@@ -117,45 +128,53 @@ function change_blocks(ast, scopeManager) {
             var currunt_variables = [];
             var current_scope = scopeManager.acquire(parent);
 
-            if (current_scope) {
-                currunt_variables = current_scope.variables;
-            }
+            if (current_scope) currunt_variables = current_scope.variables;
 
-            var scopeManagerNewNode = escope.analyze(source_tree);
-            var new_node_variables = scopeManagerNewNode.acquire(new_node);
-            console.log(new_node_variables);
+            console.log(new_node);
 
-            estraverse.traverse(new_node, {
+            new_node_variables = new_node.variables;
+
+            estraverse.traverse(new_node.node, {
                 enter: function (node, parent) {
                     if (node.object) {
                         if (node.object.type == "Identifier") {
-                            if (!check_variable_existence(global_variables, node.object.name)) {
+                            if (
+                                !check_variable_existence(global_variables, node.object.name) &&
+                                !check_variable_existence(currunt_variables, node.object.name) &&
+                                !check_variable_existence(new_node_variables, node.object.name)
+                                ) {
                                 node.object.name = randomChoice(global_variables).name;
                             };
                         }
                     }
                     if (node.left) {
                         if (node.left.type == "Identifier") {
-                            if (!check_variable_existence(global_variables, node.left.name)) {
+                            if (
+                                !check_variable_existence(global_variables, node.left.name) &&
+                                !check_variable_existence(currunt_variables, node.left.name) &&
+                                !check_variable_existence(new_node_variables, node.left.name)
+                                ) {
                                 node.left.name = randomChoice(global_variables).name;
                             };
                         }
                     }
                     if (node.right) {
                         if (node.right.type == "Identifier") {
-                            if (!check_variable_existence(global_variables, node.right.name)) {
+                            if (
+                                !check_variable_existence(global_variables, node.right.name) &&
+                                !check_variable_existence(currunt_variables, node.right.name) &&
+                                !check_variable_existence(new_node_variables, node.right.name)
+                                ) {
                                 node.right.name = randomChoice(global_variables).name;
                             };
                         }
-
                     }
                 },
                 leave: function (node, parent) {}
             });
 
-            console.log(escodegen.generate(new_node));
-            process.exit(0);
-            return new_node;
+            console.log(escodegen.generate(new_node.node));
+            return new_node.node;
 
         }
     })
