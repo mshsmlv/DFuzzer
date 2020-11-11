d9b42f8fe325e8f21298d760243f46e135252648
MashaSamoylova <mashanedyak@gmail.com>
2020-11-12 02:38:08 +0700

    Do some staff

--- mutator/node_generator.js
+++ mutator/node_generator.js
@@ -5,14 +5,6 @@ var escodegen = require('escodegen');
 
 // https://esprima.readthedocs.io/en/latest/syntax-tree-format.html - вся инфа по возможным нодам
 
-let binary_operator = ['+','-','*','/','%','**','&','|','^','<<','>>','>>>']; // Math operator
-let binary_condition = ['==','!=','<','<=','>','>=','===','!==','instanceof','in']; // condition operator
-let assign_operator = ['+=','-=','*=','**=','/=','%=','&=','^=','|=','<<=','>>=','>>>=','='];
-let boolean_value = ['false','true'];
-let unary_operator = ['~','-','!','++','--','+','']; // '...' 'typeof'
-let update_operator = ['++','--'];
-let logical_operator = ['&&','||'];
-
 function randomChoice(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
@@ -21,37 +13,26 @@ function randomChoice(arr) {
 // ./data-set/basic/doMath.js 0
 // ./data-set/basic/testMulOverflow.js 0
 // ./data-set/basic/testSwitchString.js 1
+// ./data-set/basic/bug686296.js 0 где не работает scope.
 
-// TODO еще применимость этого дерева нужно в контекст прифигачить.
-// типо если в блоке continue то его нельзя пихать.
-// то есть замутить контексты.
+// ./data-set/basic/testPrimitiveConstructorPrototype.js 2
+
 
 function __get_node(ast, aimed_type, index) {
-    var scopeManager = escope.analyze(ast);
-        var res_nodes = []
-        estraverse.traverse(ast, {
-            enter: function (node, parent) {
-                if (node.type == aimed_type) {
-                    var variables = []
-                    if (parent) {
-                        var parent_scope =  scopeManager.acquire(parent);
-                        if (parent_scope) {
-                            variables = parent_scope.variables;
-                        }                  
-                    } 
-                    res_nodes.push(
-                    {
-                        node: node,
-                        variables: variables, 
-                    })
-                }
-            },
-            leave: function (node, parent) {
+    var res_nodes = []
+    estraverse.traverse(ast, {
+        enter: function (node, parent) {
+            if (node.type == aimed_type) {
+                res_nodes.push(node);
             }
-        });
-        if (!index) {
-            index = Math.floor(Math.random() * res_nodes
-            .length);
+        },
+        leave: function (node, parent) {
+        }
+    });
+
+    if (!index) {
+        index = Math.floor(Math.random() * res_nodes
+        .length);
         };
 
         console.log("index:", index);
@@ -74,7 +55,6 @@ function getNode(aimed_type) {
             return [new_node, source_tree];
         }
         continue;
-         
     }   
 }
 
@@ -94,114 +74,144 @@ function check_variable_existence(var_array, varname) {
     return false;
 }
 
-function change_blocks(ast, scopeManager) {
-    var global_variables = scopeManager.acquire(ast).variables;
+function extract_variable_names(var_map, var_array) {
+    for(var i = 0; i < var_array.length; i++) {
+        var_map.set(var_array[i].name, 1);
+    }
+    return var_map;
+}
+
+// mutate_blocks replaces blocks:
+// "ForStatement":
+// "ForInStatement":
+// "IfStatement":
+// "DoWhileStatement":
+// "SwitchStatement":
+// "WhileStatement":
+// "WithStatement":
+// "BlockStatement":
+//
+// It queries new block with the same type from the given data-set.
+function mutate_blocks(ast, scopeManager) {
+    var current_variables = new Map();
+
+
+    // эта херня еще названия функций включает
+    // эта херня еще включает все переменные, которые объявлены ниже!!!!!!
+    extract_variable_names(current_variables, scopeManager.acquire(ast).variables);
 
     estraverse.replace(ast, {
         enter: function (node, parent) {
-            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
             switch (node.type) {
-                case "FunctionDeclaration":
-                case  "ReturnStatement":
-                case  "VariableDeclaration":
-                case  "Program":
-                case  "ClassDeclaration":
-                case  "Literal":
-                case  "Identifier":
-                case  "VariableDeclarator": return;
+                case "ForStatement":
+                case  "ForInStatement":
+                case  "IfStatement":
+                case  "DoWhileStatement":
+                case  "SwitchStatement":
+                case  "WhileStatement":
+                case  "WithStatement":
+                case  "BlockStatement": break; // Нужен ли нам блок стейтмент?
+                default: return;
             }
 
-            switch (Math.random()%2) {
-                case 0: return
-                case 1: // generate
-            }
-        
-           // var [new_node, source_tree] = getSpecifiedNode("./data-set/basic/testMulOverflow.js", 0, node.type);
+          //  switch (Math.random()%2) {
+            //    case 0: return
+              //  case 1: // generate
+            //}
             
-            var [new_node, source_tree] = getNode(node.type);
-            // при вставке дерева нужно проходиться по декларациям функций и классов.
-            // то есть если вызываем какую-то функцию, то ее декларацию в дерево тоже впихиваем.
+            var [new_node, source_tree] = getSpecifiedNode("./data-set/basic/testNegZero1.js", 0, node.type);
+            var sourceScopeManager = escope.analyze(source_tree);
+
+           // var [new_node, source_tree] = getNode(node.type);
+
+
+            // Get everything what we can !!!!!!
+            var current_parent_scope = scopeManager.acquire(parent);
+            var current_node_scope = scopeManager.acquire(node);
+
+            if (current_parent_scope) {
+                extract_variable_names(current_variables, current_parent_scope.variables);
+            }
+            if (current_node_scope) {
+                extract_variable_names(current_variables, current_node_scope.variables);
+            }
+
+            // change variables name in new node if they are not declarated to the declarated variables:
+            // it can be:
+            // - global variable from mutated ast
+            // - variable which are accsessible for the current context(visible in a function or in a loop)
+            // 
+            // OR leave variable name if it is declarated in new node
             //
-            
-           
-            var currunt_variables = [];
-            var current_scope = scopeManager.acquire(parent);
+            // 
+            // Если мы идем вглубь дерева, а мы идем вглубь, то видимость переменных сохраняется,
+            // просто добавляются новые.
 
-            if (current_scope) currunt_variables = current_scope.variables;
-
-            console.log(new_node);
-
-            new_node_variables = new_node.variables;
-
-            estraverse.traverse(new_node.node, {
+            new_node_variables = new Map();
+            estraverse.traverse(new_node, {
                 enter: function (node, parent) {
+                    // returns list of variables which are declarated in a node.
+                    // See https://github.com/estools/escope/blob/49a00b8b41c8d6221446bbf6b426d1ea64d80d00/src/scope-manager.js#L98
+                    extract_variable_names(new_node_variables, sourceScopeManager.getDeclaredVariables(node));
                     if (node.object) {
                         if (node.object.type == "Identifier") {
-                            if (
-                                !check_variable_existence(global_variables, node.object.name) &&
-                                !check_variable_existence(currunt_variables, node.object.name) &&
-                                !check_variable_existence(new_node_variables, node.object.name)
-                                ) {
-                                node.object.name = randomChoice(global_variables).name;
+                            if (!current_variables.get(node.object.name) && !new_node_variables.get(node.object.name)) {
+                                node.object.name = randomChoice(Array.from(current_variables.keys()));
                             };
                         }
                     }
                     if (node.left) {
                         if (node.left.type == "Identifier") {
-                            if (
-                                !check_variable_existence(global_variables, node.left.name) &&
-                                !check_variable_existence(currunt_variables, node.left.name) &&
-                                !check_variable_existence(new_node_variables, node.left.name)
-                                ) {
-                                node.left.name = randomChoice(global_variables).name;
+
+                            if (!current_variables.get(node.left.name) && !new_node_variables.get(node.left.name)) {
+                                node.left.name = randomChoice(Array.from((current_variables.keys())));
                             };
                         }
                     }
                     if (node.right) {
                         if (node.right.type == "Identifier") {
-                            if (
-                                !check_variable_existence(global_variables, node.right.name) &&
-                                !check_variable_existence(currunt_variables, node.right.name) &&
-                                !check_variable_existence(new_node_variables, node.right.name)
-                                ) {
-                                node.right.name = randomChoice(global_variables).name;
+                            if (!current_variables.get(node.right.name) && !new_node_variables.get(node.right.name)) {
+                                node.right.name = randomChoice(Array.from(current_variables.keys()));
                             };
                         }
+                    }
+                    if (node.argument) {
+                        if (node.argument.type == "Identifier") {
+                            if (!current_variables.get(node.argument.name) && !new_node_variables.get(node.argument.name)) {
+                                node.argument.name = randomChoice(Array.from(current_variables.keys()));
+                            };
+                        } 
                     }
                 },
                 leave: function (node, parent) {}
             });
-
-            console.log(escodegen.generate(new_node.node));
-            return new_node.node;
+            return new_node;
 
         }
     })
 }
 
-function mutate(ast, scopeManager) {
+let binary_operator = ['+','-','*','/','%','**','&','|','^','<<','>>','>>>']; // Math operator
+let binary_condition = ['==','!=','<','<=','>','>=','===','!==','instanceof','in']; // condition operator
+let assign_operator = ['+=','-=','*=','**=','/=','%=','&=','^=','|=','<<=','>>=','>>>=','='];
+let boolean_value = ['false','true'];
+let unary_operator = ['~','-','!','++','--','+','']; // '...' 'typeof'
+let update_operator = ['++','--'];
+let logical_operator = ['&&','||'];
+
+// mutate_expressions just changes one simple expression to another one.
+function mutate_expressions(ast, scopeManager) {
     estraverse.traverse(ast, {
         enter: function (node, parent) {
-            if (node.type == 'BinaryExpression') {
-                node.operator = randomChoice(binary_operator);
-            }
-            if (node.type == 'LogicalExpression') {
-                node.operator = randomChoice(logical_operator);
-            }
-            if (node.type == 'AssignmentExpression') {
-                node.operator = randomChoice(assign_operator);
-            }
-            if (node.type == 'UnaryExpression') {
-                node.operator = randomChoice(unary_operator);
-            }
-            if (node.type == 'UpdateExpression') {
-                node.operator = randomChoice(update_operator);
+            switch(node.type) {
+                case "BinaryExpression": node.operator = randomChoice(binary_operator);
+                case "LogicalExpression": node.operator = randomChoice(logical_operator);
+                case "AssignmentExpression": node.operator = randomChoice(assign_operator);
+                case "UnaryExpression":  node.operator = randomChoice(unary_operator);
+                case "UpdateExpression": node.operator = randomChoice(update_operator);
             }
         },
         leave: function (node, parent) {
-            if (node.type == 'VariableDeclarator')
-              console.log(node.id.name);
         }
     });
 };
@@ -214,7 +224,7 @@ function mutate_code(code) {
     var ast = esprima.parse(code);
     var scopeManager = escope.analyze(ast);
   
-    change_blocks(ast, scopeManager);
+    mutate_blocks(ast, scopeManager);
     return escodegen.generate(ast);
 }
 
